7ca8e52a5ffc22bbaa417f09c0d7dc21735337ed
João Vítor Motta Souto Maior <soutomaior950@gmail.com>
2026-08-18 01:45:07 -0300

    feat: add Heroku deployment support with PostgreSQL SSL and sharp build

--- config/plugins.js
+++ config/plugins.js
@@ -6,4 +6,9 @@ module.exports = ({ env }) => ({
       enabled: true,
       resolve: './src/plugins/pagamento'
     },
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'default-jwt-secret-change-me'),
+      },
+    },
   });
